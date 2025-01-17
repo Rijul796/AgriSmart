0f0a6a4d505b335fd1ada67f48402c5641efdfdc
Rijul Sharma <118725579+Rijul796@users.noreply.github.com>
2025-01-17 16:56:59 +0530

    17 Jan Last Update
    I've added the tab bar for the buyers flow as per the screens still work is to be done on the present godly and show segue

--- AgriSmart_12/AgriSmart_12/FarmerViewController.swift
+++ AgriSmart_12/AgriSmart_12/FarmerViewController.swift
@@ -24,21 +24,6 @@ class FarmerViewController: UIViewController {
         }
     }
     
-    
-    @IBAction func signupbtnTapped(_ sender: UIButton) {
-        let storyboard = UIStoryboard(name: "Farmer", bundle: nil)
-        storyboard.instantiateViewController(withIdentifier: "homeSc") as! HomeScreenViewController
-    
-    }
-    
-    /*
-    // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
