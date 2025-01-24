af6160a78e5c66e301fc970284dbf227314fb758
Rijul Sharma <rijulsharma.1972@gmail.com>
2025-01-25 05:05:04 +0530

    search functionality added in chats and help desk

--- AgriSmart_12/ConsumerScreenFlow/ChatViewController.swift
+++ AgriSmart_12/ConsumerScreenFlow/ChatViewController.swift
@@ -1,27 +1,35 @@
 import UIKit
 
-class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var SearchBar: UISearchBar!
 
     var chats = [
         (username: "Pankaj", message: "Thank you! That was very helpful!", imageName: "farmer-1"),
         (username: "Ram", message: "Was it available?", imageName: "Farmer-2"),
         (username: "Shyam", message: "2kg beans are available.", imageName: "farmer-3")
     ]
+    
+    var filteredChats: [(username: String, message: String, imageName: String)] = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.dataSource = self
         tableView.delegate = self
+        SearchBar.delegate = self
+        
+        // Initialize the filteredChats array with all chats initially
+        filteredChats = chats
     }
 
+    // MARK: - TableView DataSource Methods
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return chats.count
+        return filteredChats.count
     }
 
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "ChatCell", for: indexPath) as! ChatCell
-        let chat = chats[indexPath.row]
+        let chat = filteredChats[indexPath.row]
         cell.usernameLabel.text = chat.username
         cell.messageLabel.text = chat.message
         cell.profileImageView.image = UIImage(named: chat.imageName)
@@ -31,30 +39,53 @@ class ChatViewController: UIViewController, UITableViewDataSource, UITableViewDe
         return cell
     }
 
+    // MARK: - Search Bar Delegate Methods
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            // Show all chats if the search text is empty
+            filteredChats = chats
+        } else {
+            // Filter chats based on the search text
+            filteredChats = chats.filter { chat in
+                chat.username.lowercased().contains(searchText.lowercased()) ||
+                chat.message.lowercased().contains(searchText.lowercased())
+            }
+        }
+        tableView.reloadData()
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        // Reset the search bar and display all chats
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+        filteredChats = chats
+        tableView.reloadData()
+    }
+
+    // MARK: - Navigation and Delete Functionality
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        // Trigger segue to SubChat view
         performSegue(withIdentifier: "subChat", sender: indexPath)
     }
 
-    // Prepare data before the segue is performed
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "subChat", let indexPath = sender as? IndexPath {
-            // Pass data to the DetailChatViewController
             let subChatVC = segue.destination as! DetailChatViewController
-            let chat = chats[indexPath.row]
-            subChatVC.userName = chat.username  // Pass the user name
-            subChatVC.userImage = UIImage(named: chat.imageName)  // Pass the user image
+            let chat = filteredChats[indexPath.row]
+            subChatVC.userName = chat.username
+            subChatVC.userImage = UIImage(named: chat.imageName)
         }
     }
 
-
-    // Add swipe-to-delete functionality
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            // Remove the chat from the data source
-            chats.remove(at: indexPath.row)
+            let chatToDelete = filteredChats[indexPath.row]
 
-            // Delete the row from the table view with animation
+            // Remove from both filteredChats and chats array
+            if let indexInOriginalArray = chats.firstIndex(where: { $0.username == chatToDelete.username && $0.message == chatToDelete.message }) {
+                chats.remove(at: indexInOriginalArray)
+            }
+            filteredChats.remove(at: indexPath.row)
+
             tableView.deleteRows(at: [indexPath], with: .fade)
         }
     }
--- AgriSmart_12/ConsumerScreenFlow/ConsumerHelpDeskViewController.swift
+++ AgriSmart_12/ConsumerScreenFlow/ConsumerHelpDeskViewController.swift
@@ -1,25 +1,10 @@
-//
-//  ConsumerHelpDeskViewController.swift
-//  AgriSmart_12
-//
-//  Created by student-2 on 20/01/25.
-//
-
-
-//
-//  ConsumerHelpDeskViewController.swift
-//  home_page_apex
-//
-//  Created by student-2 on 19/12/24.
-//
-
 import UIKit
 
-class ConsumerHelpDeskViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-    
+class ConsumerHelpDeskViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
+
     @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var searchBar: UISearchBar!
     
-    // Consumer-specific help topics and details
     let helpTopics = [
         "How to search for products?",
         "How to place an order?",
@@ -34,35 +19,49 @@ class ConsumerHelpDeskViewController: UIViewController, UITableViewDelegate, UIT
     ]
     
     var expandedCells: Set<Int> = [] // To keep track of expanded cells
-    
+    var filteredHelpTopics: [String] = [] // For search functionality
+    var filteredHelpDetails: [String] = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         tableView.delegate = self
         tableView.dataSource = self
+        searchBar.delegate = self
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "helpCell")
+        
+        // Initialize filtered arrays with all data
+        filteredHelpTopics = helpTopics
+        filteredHelpDetails = helpDetails
     }
-    
+
     // MARK: - TableView DataSource Methods
-    
+
     func numberOfSections(in tableView: UITableView) -> Int {
         return 1
     }
-    
+
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return helpTopics.count
+        return filteredHelpTopics.count
     }
-    
+
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "helpCell", for: indexPath)
-        cell.textLabel?.text = helpTopics[indexPath.row]
-        cell.textLabel?.numberOfLines = expandedCells.contains(indexPath.row) ? 0 : 1 // Expand or collapse the cell text
+        let topic = filteredHelpTopics[indexPath.row]
+        let detail = filteredHelpDetails[indexPath.row]
         
+        if expandedCells.contains(indexPath.row) {
+            cell.textLabel?.text = "\(topic)\n\n\(detail)"
+        } else {
+            cell.textLabel?.text = topic
+        }
+        
+        cell.textLabel?.numberOfLines = expandedCells.contains(indexPath.row) ? 0 : 1 // Expand or collapse text
         return cell
     }
-    
+
     // MARK: - TableView Delegate Methods
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
         
@@ -74,7 +73,7 @@ class ConsumerHelpDeskViewController: UIViewController, UITableViewDelegate, UIT
         
         tableView.reloadRows(at: [indexPath], with: .automatic)
     }
-    
+
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         if expandedCells.contains(indexPath.row) {
             return UITableView.automaticDimension // Dynamic height for expanded cell
@@ -82,8 +81,39 @@ class ConsumerHelpDeskViewController: UIViewController, UITableViewDelegate, UIT
             return 44.0 // Standard height for collapsed cell
         }
     }
-    
+
     func tableView(_ tableView: UITableView, estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
         return 44.0
+    }
+
+    // MARK: - Search Bar Delegate Methods
+
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText.isEmpty {
+            // Show all topics and details if search bar is empty
+            filteredHelpTopics = helpTopics
+            filteredHelpDetails = helpDetails
+        } else {
+            // Filter topics and details based on search text
+            filteredHelpTopics = helpTopics.enumerated().filter { index, topic in
+                topic.lowercased().contains(searchText.lowercased()) ||
+                helpDetails[index].lowercased().contains(searchText.lowercased())
+            }.map { $0.element }
+            
+            filteredHelpDetails = helpTopics.enumerated().filter { index, topic in
+                topic.lowercased().contains(searchText.lowercased()) ||
+                helpDetails[index].lowercased().contains(searchText.lowercased())
+            }.map { helpDetails[$0.offset] }
+        }
+        tableView.reloadData()
+    }
+
+    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
+        // Reset the search bar and show all data
+        searchBar.text = ""
+        searchBar.resignFirstResponder()
+        filteredHelpTopics = helpTopics
+        filteredHelpDetails = helpDetails
+        tableView.reloadData()
     }
 }
